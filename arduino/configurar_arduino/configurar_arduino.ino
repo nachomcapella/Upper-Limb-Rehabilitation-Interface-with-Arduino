343834e21fb3a43808eb30cdbaee16584bbc375a
Ignacio Martínez Capella <nachetemc97@gmail.com>
2018-01-18 15:36:17 +0100

    Add files via upload

---
+++ arduino/configurar_arduino/configurar_arduino.ino
@@ -0,0 +1,36 @@
+
+char nombre[21] = "arduino";
+char bps = '4';
+char contrasena[5] = "1234";
+int led1 = 13;
+int delay1 = 10000;
+int delay2 = 1000;
+int delay3 = 500;
+
+void setup() {
+Serial1.begin(9600); //Serial1 works with Arduino Micro as SofwareSerial does with Uno.
+pinMode(led1,OUTPUT);
+digitalWrite(led1,HIGH);
+delay(delay1);
+digitalWrite(led1,LOW);
+
+Serial1.print("AT"); //Comienza la configuración.
+delay(delay2);
+
+Serial1.print("AT+NAME"); //Da nombre al bluetooth.
+Serial1.print(nombre);
+delay(delay2);
+
+Serial1.print("AT+BAUD"); //Define una velocidad de trabajo.
+Serial1.print(bps);
+delay(delay2);
+
+Serial1.print("AT+PIN"); //Establece la contraseña.
+Serial1.print(contrasena);
+delay(delay2);
+}
+
+void loop() {
+  digitalWrite(led1, !digitalRead(led1)); //El led parpadea.
+  delay(delay3);
+}
---
+++ arduino/prueba-rn42-v2/prueba-rn42-v2.ino
@@ -0,0 +1,110 @@
+//char msg;
+//int th; //Threslhold.
+//int sensor1 = 5;
+//int sensor2 = 4;
+//int led1 = 13;
+//int sensor1Reading;
+//int sensor2Reading;
+//int delay1 = 100;
+//
+//void setup()
+//{
+//  Serial1.begin(9600);  // Begin the serial monitor at 9600bps
+//  delay(100);
+//  Serial1.println("Hello from Arduino"); // Send test message to other device.
+//  pinMode(led1, OUTPUT);
+//  th = 1023;
+//}
+//void loop() {
+//  //BT message.
+//  if (Serial1.available()) { //If text arrived in from BT serial...
+//    msg = (Serial1.read());
+//    if (msg == '1')
+//    {
+//      digitalWrite(led1, HIGH);
+//    }
+//
+//    if (msg == '2')
+//    {
+//      digitalWrite(led1, LOW);
+//    }
+//  }
+//}
+#include<Mouse.h>
+char msg;
+int th; //Threslhold.
+int sensor1 = 5;
+int sensor2 = 4;
+int led1 = 13;
+int sensor1Reading;
+int sensor2Reading;
+int delay1 = 100;
+
+void setup() {
+  Serial1.begin(9600);
+  Mouse.begin(); //Enables mouse control.
+  Serial1.println("Hello from Arduino"); // Send test message to other device.
+  pinMode(led1, OUTPUT);
+  th = 1023;
+  delay(10 * delay1);
+}
+
+void loop() {
+  //BT message.
+  if (Serial1.available()) { //If text arrived in from BT serial...
+    msg = (Serial1.read());
+    if (msg == '1')
+    {
+      th = 400;
+      Serial1.println("");
+      Serial1.print("Threshold updated: ");
+      Serial1.println(th);
+      Serial1.println("");
+      delay(3 * delay1);
+    }
+    if (msg == '2')
+    {
+      th = 800;
+      Serial1.println("");
+      Serial1.print("Threshold updated: ");
+      Serial1.println(th);
+      Serial1.println("");
+      delay(3 * delay1);
+
+    }
+    if (msg == '?')
+    {
+      Serial1.println("");
+      Serial1.println("Send '1' to set the threshold to 400.");
+      Serial1.println("Send '2' to set the threshold to 800.");
+      Serial1.print("Actual threshold: ");
+      Serial1.println(th);
+      Serial1.println("");
+      delay(3 * delay1);
+    }
+  }
+
+  //SENSOR
+  sensor1Reading = analogRead(sensor1);
+  sensor2Reading = analogRead(sensor2);
+  Serial1.print("Sensor 1 = ");
+  Serial1.println(sensor1Reading);
+  Serial1.print("Sensor 2 = ");
+  Serial1.println(sensor2Reading);
+  Serial1.println("");
+  if (sensor1Reading > th) {
+    digitalWrite(led1, HIGH);
+    Mouse.click(MOUSE_LEFT);
+    delay(delay1);
+  } else {
+    digitalWrite(led1, LOW);
+  }
+  if (sensor2Reading > th) {
+    digitalWrite(led1, HIGH);
+    Mouse.click(MOUSE_RIGHT);
+    delay(delay1);
+  } else {
+    analogWrite(led1, LOW);
+  }
+  delay(delay1);
+}
