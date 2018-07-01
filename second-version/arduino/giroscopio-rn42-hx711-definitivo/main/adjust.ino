3de25862fa8c8aca4bf89b3f2d5275e63bfe7bcc
Ignacio Martínez Capella <nachetemc97@gmail.com>
2018-07-01 23:07:55 +0200

    Add files via upload

---
+++ second-version/arduino/giroscopio-rn42-hx711-definitivo/main/adjust.ino
@@ -0,0 +1,28 @@
+#include<Mouse.h>
+
+void clic() {
+
+  //HX711
+  units = 0.00; //Reset the value to avoid false "clicks".
+  scale.set_scale(calibration_factor); //Adjust to this calibration factor
+  Serial.print("Reading: ");
+  units = scale.get_units(), 10;
+  if (units < 0)
+  {
+    units = 0.00;
+  }
+  Serial.print(units);
+  Serial.print(" grams");
+  Serial.print(" calibration_factor: ");
+  Serial.print(calibration_factor);
+  Serial.println();
+  
+  if (Serial.available())
+  {
+    char temp = Serial.read();
+    if (temp == '+' || temp == 'a')
+      calibration_factor += 1;
+    else if (temp == '-' || temp == 'z')
+      calibration_factor -= 1;
+  }
+}
